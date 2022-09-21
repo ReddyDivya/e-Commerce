35bd542df01e1352b39e39784bd916882cbf8986
Divya <divyareddy0205@gmail.com>
2022-09-21 12:52:53 -0700

    updating navbar

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,9 +4,9 @@ import CartItem from "../Cart/CartItem";
 export default function Cart({ cart }) {
     return (
         <div>
-            <div className="m-4 p-3 w-full bg-white rounded-lg border shadow-md">
+            <div className="m-10 p-3 w-2/4 bg-white rounded-lg border shadow-md">
                 <div className="flex justify-between items-center mb-4">
-                    <h5 className="text-xl font-bold text-gray-900"> Cart Items</h5>
+                    <h5 className="text-xl font-bold text-gray-900">Cart Items</h5>
                     <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
                         View all
                     </a>
@@ -21,9 +21,7 @@ export default function Cart({ cart }) {
                     </ul>
                 </div>
             </div>
-            <div>
-                <button className="font-medium bg-blue-700 px-2 py-2.5">Remove</button>
-            </div>
+
         </div>
     )
 }
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,27 +1,35 @@
 import React from 'react'
 
 export default function CartItem({ item }) {
-    console.log(Object.value(item.media.img))
+    // console.log(Object.keys(item.length))
+    const EmptyCart = () => {
+        <h3>You have no items in your shopping cart, start adding some!</h3>
+    }
+
     return (
-        <div>
-            <li className="py-3 sm:py-4">
-                <div className="flex items-center space-x-4">
+
+        < div >
+            <li className="py-3 sm:py-4 items-center">
+                <div className="flex items-center">
                     <div className="flex-shrink-0">
-                        <img className="w-8 h-8 rounded-full" src={item.media.img} alt="item" />
+                        <img className="w-8 h-8 rounded" src={item.image.url} alt={item.name} />
                     </div>
-                    <div className="flex-1 min-w-0">
-                        <p className="text-sm font-medium text-gray-900 dar:text-white">{item.name}</p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                        <p>{item.line_total.formatted_with_symbol}</p>
+                    <div className="flex-1 m-4 p-2">
+                        <p className="text-sm font-bold text-gray-900 dar:text-white">{item.name}</p>
                     </div>
                     <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                        <button className="bg-blue-700 font-medium px-5 pl-2 py-2.5 items-center">+</button>
-                        <p>{item.quantity}</p>
-                        <button className="bg-blue-700 font-medium px-5 py-2.5 items-center">-</button>
+                        <p className="p-4">{item.line_total.formatted_with_symbol}</p>
+                    </div>
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <button className="bg-blue-700 font-bold p-4 items-center rounded-full text-white">+</button>
+                        <p className="p-3">{item.quantity}</p>
+                        <button className="bg-blue-700 font-bold p-4 items-center rounded-full text-white">-</button>
+                    </div>
+                    <div className="m-3">
+                        <button className="font-medium bg-blue-700 px-2 py-2.5 rounded text-white">Remove</button>
                     </div>
                 </div>
             </li>
-        </div>
+        </div >
     )
 }
